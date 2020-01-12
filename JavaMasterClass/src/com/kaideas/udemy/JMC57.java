277b43cec3d09cbee01b6c62232e1d8442e0f354
Kaigh <7100874+krazykaigh@users.noreply.github.com>
2020-01-12 08:45:38 -0500

    Rejected Push from Intellij fixed
    This is the initial commit of the Java Master Class for Software Developers Udemy Course

---
+++ JavaMasterClass/src/com/kaideas/udemy/JMC57.java
@@ -0,0 +1,37 @@
+package com.kaideas.udemy;
+
+public class JMC57 {
+
+  public static void main(String[] args) {
+    System.out.println("centimeters = " +
+        calcFeetAndInchesToCentimeters(72));
+    System.out.println("centimeters = " +
+        calcFeetAndInchesToCentimeters(99));
+    System.out.println("centimeters = " +
+        calcFeetAndInchesToCentimeters(-1));
+  }
+
+  public static double calcFeetAndInchesToCentimeters(double feet, double inches){
+    if ((feet < 0) || (inches < 0 || inches >12)){
+      return -1;
+    }
+    // convert parameters to inches only
+    inches =+ feet * 12;
+    // return centimeters
+    return inches * 2.54;
+  }
+
+  public static double calcFeetAndInchesToCentimeters(double inches){
+    if (inches < 0)
+      return -1;
+
+    int feet = (int)inches / 12;
+    System.out.println("inches = " +inches );
+    System.out.println("feet = " + feet);
+    inches = (inches % 12);
+    System.out.println("remaining inches = " + inches);
+    // return centimeters
+    return calcFeetAndInchesToCentimeters (feet, inches);
+  }
+}
+
